1da2e4ade897b92dddc7733317b7a147ed34714b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-11-05 11:34:02 +0530

    Added Env Vars

--- Game/menu.py
+++ Game/menu.py
@@ -10,11 +10,13 @@ import subprocess
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+
 class Button():
     """
     Class to create a new button in pygame window.
     """
     # initialise the button
+
     def __init__(self, colour, x, y, width, height, text=""):
         self.colour = colour
         self.x = x
@@ -41,6 +43,7 @@ class Button():
                 return True
 
         return False
+
 
 def drawRoundRect(surface, colour, rect, radius=0.4):
     """
@@ -81,51 +84,57 @@ def drawRoundRect(surface, colour, rect, radius=0.4):
 
     surface.blit(rectangle, pos)
 
-def showMenu(screen, SCREEN_WIDTH):
+
+def showMenu(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
     level_0 = Button(
-    tuple([237,194,46]), 200-70, 275, 45, 45, "Level 0")
+        tuple([237, 194, 46]), SCREEN_WIDTH // 2 - 300 - 150, SCREEN_HEIGHT // 2 - 500 , 300, 300, "Level 0")
     level_1 = Button(
-    tuple([237,194,46]), 270-70, 275, 45, 45, "Level 1")
+        tuple([237, 194, 46]), SCREEN_WIDTH // 2 + 300 - 150, SCREEN_HEIGHT // 2 - 500 , 300, 300, "Level 1")
     running = True
     current_level = 0
     level_selected = False
-    play = Button(tuple([237,194,46]),
-                  235, 400, 45, 45, "play")
+    play = Button(tuple([237, 194, 46]),
+                  SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2, 300, 300, "play")
     while running:
-            for event in pygame.event.get():
-                pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
 
-                if event.type == QUIT:
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
                     running = False
-                elif event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if level_0.isOver(pos):
-                        current_level = 0
-                        level_selected = True
-                    
-                    if level_1.isOver(pos):
-                        current_level = 1
-                        level_selected = True
-                    
-                    if play.isOver(pos):
-                        if level_selected != False:
-                            subprocess.call("python /home/shogo/master/qc/Zenith-zealots/Game/game.py", shell=True)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if level_0.isOver(pos):
+                    level_0.colour = tuple([246,94,59])
+                    current_level = 0
+                    level_selected = True
 
-            screen.fill(BLACK)
-            font = pygame.font.SysFont("Verdana", 69, bold=True)
-            font1 = pygame.font.SysFont("Verdana", 69, bold=True)
+                if level_1.isOver(pos):
+                    level_1.colour = tuple([246,94,59])
+                    current_level = 1
+                    level_selected = True
 
-            theme_text = font.render("WELCOME ", 1, WHITE)
-            screen.blit(theme_text, (200, 200))
-            level_0.draw(screen, BLACK, font1)
-            level_1.draw(screen, BLACK, font1)
-            play.draw(screen, BLACK, font1)
+                if play.isOver(pos):
+                    play.colour = tuple([246,94,59])
+                    if level_selected != False:
+                        subprocess.call(
+                            "python /home/shogo/master/qc/Zenith-zealots/Game/game.py", shell=True)
 
-            pygame.display.update()
+        screen.fill(BLACK)
+        font = pygame.font.SysFont("Verdana", 69, bold=True)
+        font1 = pygame.font.SysFont("Verdana", 69, bold=True)
 
-            pygame.display.flip()
+        theme_text = font.render("WELCOME ", 1, WHITE)
+        screen.blit(theme_text, (SCREEN_WIDTH / 2 - 150, 200))
+        level_0.draw(screen, BLACK, font1)
+        level_1.draw(screen, BLACK, font1)
+        play.draw(screen, BLACK, font1)
+
+        pygame.display.update()
+
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     pygame.init()
@@ -134,6 +143,6 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Zenith Zealots")
 
-    showMenu(screen, SCREEN_WIDTH)
+    showMenu(screen, SCREEN_WIDTH,SCREEN_HEIGHT)
 
     pygame.quit()
